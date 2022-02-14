3e5fd5fde08e0c6b615f8d38f9cb52ba8375cda9
p2dy <patrick-friedrich@web.de>
2022-02-14 22:30:14 +0100

    add missing @ApplicationScoped Annotation

--- board/src/main/java/com/example/board/domain/BoardIdProvider.java
+++ board/src/main/java/com/example/board/domain/BoardIdProvider.java
@@ -1,7 +1,9 @@
 package com.example.board.domain;
 
+import javax.enterprise.context.ApplicationScoped;
 import java.util.UUID;
 
+@ApplicationScoped
 public class BoardIdProvider {
     public BoardId generate() {
         return BoardId.of(UUID.randomUUID());
